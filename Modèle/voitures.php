3d258a82349eb4b938c8f870446059e21daf6d20
DESKTOP-BGFT4SH\samst <samstp98@msn.com>
2019-02-27 10:36:24 -0500

    voiture

---
+++ Modèle/voitures.php
@@ -0,0 +1,12 @@
+<?php
+
+$tab_marqueHonda = array(
+    ('modele' => 'civic', 'prix' => 3000, 'descritpion' => "Honda civic 2003 bleu avec 4 pneus d'hiver pirelli neufs tout fonctionne"),
+    ('modele' => 'accord', 'prix' => 4999, 'descritpion' => "Honda accord 2002 v6 gris, tout équipé, faut que sa parte !"),
+    ('modele' => 'cr-v', 'prix' => 2499, 'descritpion' => "Honda Cr-V 2004 v4 rouge, fonctionne très bien, appartenait à une mère de famille, 1 seul proprio !"));
+
+$tab_marqueToyota = array(
+    ('modele' => 'camry', 'prix' => 6000, 'descritpion' => "Toyota camry 2003 v4 noir, super bonne voiture, traité à l'antirouille"),
+    ('modele' => 'corolla', 'prix' => 5499, 'descritpion' => "Honda civic 2003 bleu avec 4 pneus d'hiver pirelli neufs tout fonctionne"),
+)
+?>
