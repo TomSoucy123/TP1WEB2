6192ce04ed6c4912e2820f4e02f70b7729adcb67
TomSoucy123 <tiyo_tom@hotmail.com>
2019-02-27 11:40:03 -0500

    Update index.php
    Function

--- Controleur/index.php
+++ Controleur/index.php
@@ -1,47 +1,83 @@
 <?php
+$dureePret = 36;
+$montantFinance = 8000;
 
 function DeterminerLeTauxDinteret($dureePret, $montantFinance){
     $tauxInteret = 0;
 if($dureePret == 24 || $dureePret == 12 && $montantFinance <= 10000){
     $tauxInteret = 6.95;
+    
 
 }
 if($dureePret == 24 || $dureePret == 12 && $montantFinance > 10000){
     $tauxInteret = 7.25;
+    
 
 }
 if($dureePret == 36 && $montantFinance <= 10000){
     $tauxInteret = 6.25;
+    
+    
 }
 if($dureePret == 36 && $montantFinance > 10000){
         $tauxInteret = 6.30;
+        
+        
     
 }
 if($dureePret == 48 && $montantFinance <= 10000){
         $tauxInteret = 6.10;
+        
 }
 if($dureePret == 48 && $montantFinance > 10000){
         $tauxInteret = 6.30;
+        
     
 }
 if($dureePret == 60 && $montantFinance <= 10000){
         $tauxInteret = 6.00;
+        
 }
 if($dureePret == 60 && $montantFinance > 10000){
         $tauxInteret = 5.85;
+        
     
 }
+
 $tauxInteret = $tauxInteret/100;
+// echo $tauxInteret;
+
+
 
 return $tauxInteret;
 }
-function mensualités(){
-DeterminerLeTauxDinteret()
 
 
+function puissance($nombre,$exposant){
+$resultat=0;
+        for ($i=0;$i<$exposant;$i++){
+                $resultat =  $resultat *$nombre;
+        }
+return $resultat;
+ }
+
+
+
+
+
+$dureeDuPret = 24;
+function mensualités($dureeDuPret){
+$mensualite = 0;
+$interet = DeterminerLeTauxDinteret($dureeDuPret, $montantFinance);
 
 
 }
+
+
+
+ 
+
+} 
 
 
 
