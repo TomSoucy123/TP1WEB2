552afe7c1bb5f6bbb48a889d76272e21cce71994
TomSoucy123 <tiyo_tom@hotmail.com>
2019-03-11 15:09:26 -0400

    Update financement.php

--- Modèle/financement.php
+++ Modèle/financement.php
@@ -1,6 +1,6 @@
 <?php
-include ('\wamp64\www\Exer\TP1WEB2\Controleur\index.php');
-include ('\wamp64\www\Exer\TP1WEB2\Modèle\voitures.php');
+include ('\wamp64\www\TP1WEB2\Controleur\index.php');
+include ('\wamp64\www\TP1WEB2\Modèle\voitures.php');
 $mensualite = calcul_mensualite($dureePret,$capitalInvesti);
 echo "La mensualité est ".$mensualite; 
 echo "<br><br>";
