794f43aae0e61cacea5cb8668367c741a2f96c9a
TomSoucy123 <tiyo_tom@hotmail.com>
2019-03-11 13:11:08 -0400

    Update accueil.php
    Liste deroulante

--- Vue/accueil.php
+++ Vue/accueil.php
@@ -11,28 +11,31 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
     
 </head>
 
-<body>
-  <header>
+
+  
   <!--------------- Barre de navigation ------------------>
     <nav class="navbar navbar-light bg-light">
       <a class="navbar-brand" href="#">
         <img src="D:\wamp64\www\TP1WEB2\Vue\Dealer-Car.jpg" width="200" height="120" class="d-inline-block align-top" alt="car-brand">
       </a>
-    </nav>
-  </header>
-
+    </nav> 
+    <h1>Bienvenue sur notre site d'achat de voitures en ligne</h1><br>
+  
+    <form name='formulaire' method='post'>
   <!--------------- Liste deroulante marque ------------------>
-  <select name="liste_marque">
+  <label for='marques'> Liste De Marque <select name="liste_marque">
     <?php
-    include ("voitures.php");
-    listeMarque($tab_marqueHonda);
+    include ('voitures.php');
+
+    choisirMarques($tab_marques);
     ?>
   </select>
+  </form>
 
     
-</body>
+
 </html>
