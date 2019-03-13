d633034dffd11fc40b3c9689b323d611631dcee2
TomSoucy123 <tiyo_tom@hotmail.com>
2019-03-13 16:12:25 -0400

    Update selection.php

--- Vue/selection.php
+++ Vue/selection.php
@@ -1,13 +1,7 @@
 <?php
-include ('\wamp64\www\TP1WEB2\Modèle\voitures.php');
-echo  $_GET['marque'] . " de " .$_GET['modele'];
+/* include ('\wamp64\www\TP1WEB2\Modèle\voitures.php'); */
 
-    /* if($_POST['Honda'])
-    foreach($etudiant as $valeur){
-        echo $valeur;
-    }
-    echo '<br>';
-} */
+echo  $_GET['marque'] . " de " .$_GET['modele'];
 
 
 
