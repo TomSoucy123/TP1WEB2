0fe672851718cde19c6a19b9eb188e5a264581b9
TomSoucy123 <tiyo_tom@hotmail.com>
2019-03-13 17:07:28 -0400

    Update selection.php

--- Vue/selection.php
+++ Vue/selection.php
@@ -1,5 +1,5 @@
 <?php
-/* include ('\wamp64\www\TP1WEB2\Modèle\voitures.php'); */
+include ('\wamp64\www\TP1WEB2\Modèle\voitures.php'); 
 include ("\wamp64\www\TP1WEB2\Controleur\index.php");
 echo  $_GET['marque'] . " de " .$_GET['modele'];
 $marque =  $_GET['marque'];
