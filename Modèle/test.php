979d63a2b4f028c4a00c3ce6e4da91f3d77a30e2
TomSoucy123 <tiyo_tom@hotmail.com>
2019-03-14 13:14:16 -0400

    Update test.php

--- Modèle/test.php
+++ Modèle/test.php
@@ -1,6 +1,12 @@
 <?php
 include ("\wamp64\www\TP1WEB2\Controleur\index.php"); 
 
-echo '1- ' . calcul_mensualite(24,9000) . '<br>'; //9000''
+echo '1- ' . calcul_mensualite(24,9000) . '<br>'; 
+echo '2- ' . calcul_mensualite(60,10000) . '<br>';//169.22
+//http://www4.bmo.com/popup/prets/Calculator.html
+
+echo "<br><br>";
+echo '1- ' . calculerInterets(24,10000) . '<br>'; 
+echo '2- ' . calculerInterets(60,10000) . '<br>'; 
 
 ?>
