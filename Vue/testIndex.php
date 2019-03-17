1744a6ba5ba3b8b0d57a49a82632214fb2863f16
DESKTOP-BGFT4SH\samst <samstp98@msn.com>
2019-03-17 15:07:59 -0400

    Update testIndex.php

--- Vue/testIndex.php
+++ Vue/testIndex.php
@@ -37,30 +37,34 @@ echo '--------- test fonction determinerLeTauxDinteret($dureePret, $prix) ------
 
 echo '1-' . determinerLeTauxDinteret(12, 5000) .' -fonctionne'. '<br><br>';
 echo '2-' . determinerLeTauxDinteret(12, 10001) .' -fonctionne'. '<br><br>';
-<<<<<<< HEAD
 echo '3-' . determinerLeTauxDinteret(0, 10001) .' -fonctionne'. '<br><br>';
-echo '3-' . determinerLeTauxDinteret(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
-echo '3-' . determinerLeTauxDinteret(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
+echo '4-' . determinerLeTauxDinteret(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
+echo '5-' . determinerLeTauxDinteret(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
 
 echo '--------- test fonction trouverTauxInteret($dureePret, $prix) -----------'.'<br><br><br>';
 
 echo '1-' . trouverTauxInteret(12, 5000) .' -fonctionne'. '<br><br>';
 echo '2-' . trouverTauxInteret(12, 10001) .' -fonctionne'. '<br><br>';
 echo '3-' . trouverTauxInteret(0, 10001) .' -fonctionne'. '<br><br>';
-echo '3-' . trouverTauxInteret(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
-echo '3-' . trouverTauxInteret(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
+echo '4-' . trouverTauxInteret(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
+echo '5-' . trouverTauxInteret(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
 
 echo '--------- test fonction calcul_mensualite($dureePret,$capitalInvesti) -----------'.'<br><br><br>';
 
 echo '1-' . calcul_mensualite(12, 5000) .' -fonctionne'. '<br><br>';
 echo '2-' . calcul_mensualite(12, 10001) .' -fonctionne'. '<br><br>';
 echo '3-' . calcul_mensualite(0, 10001) .' -fonctionne'. '<br><br>';
-echo '3-' . calcul_mensualite(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
-echo '3-' . calcul_mensualite(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
+echo '4-' . calcul_mensualite(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
+echo '5-' . calcul_mensualite(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
 
 echo '--------- test fonction calculerInterets($prix,$mois,$accompte,$interet) -----------'.'<br><br><br>';
 
-echo '1-' . calculerInterets(4500,12,2000,) .' -fonctionne'. '<br><br>';
-=======
->>>>>>> 6bd495e7a0c9537794c538b489cf20fe00316ac8
+echo '1-' . calculerInterets(4500,12,2000,0.1) .' -fonctionne'. '<br><br>';
+
+echo '--------- test fonction calculerTaxes($prix) -----------'.'<br><br><br>';
+
+echo '1-' . calculerTaxes(3000) .' -fonctionne'. '<br><br>';
+echo '2-' . calculerTaxes(1000) .' -fonctionne'. '<br><br>';
+echo '3-' . calculerTaxes(10001) .' -fonctionne'. '<br><br>';
+echo '4-' . calculerTaxes(0) .' -fonctionne'. '<br><br>';
 ?>
