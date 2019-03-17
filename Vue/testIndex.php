ce135574c6be53a9d145640e8c1f80a9e9996add
DESKTOP-BGFT4SH\samst <samstp98@msn.com>
2019-03-17 14:55:59 -0400

    Update testIndex.php

--- Vue/testIndex.php
+++ Vue/testIndex.php
@@ -37,4 +37,27 @@ echo '--------- test fonction determinerLeTauxDinteret($dureePret, $prix) ------
 
 echo '1-' . determinerLeTauxDinteret(12, 5000) .' -fonctionne'. '<br><br>';
 echo '2-' . determinerLeTauxDinteret(12, 10001) .' -fonctionne'. '<br><br>';
+echo '3-' . determinerLeTauxDinteret(0, 10001) .' -fonctionne'. '<br><br>';
+echo '3-' . determinerLeTauxDinteret(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
+echo '3-' . determinerLeTauxDinteret(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
+
+echo '--------- test fonction trouverTauxInteret($dureePret, $prix) -----------'.'<br><br><br>';
+
+echo '1-' . trouverTauxInteret(12, 5000) .' -fonctionne'. '<br><br>';
+echo '2-' . trouverTauxInteret(12, 10001) .' -fonctionne'. '<br><br>';
+echo '3-' . trouverTauxInteret(0, 10001) .' -fonctionne'. '<br><br>';
+echo '3-' . trouverTauxInteret(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
+echo '3-' . trouverTauxInteret(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
+
+echo '--------- test fonction calcul_mensualite($dureePret,$capitalInvesti) -----------'.'<br><br><br>';
+
+echo '1-' . calcul_mensualite(12, 5000) .' -fonctionne'. '<br><br>';
+echo '2-' . calcul_mensualite(12, 10001) .' -fonctionne'. '<br><br>';
+echo '3-' . calcul_mensualite(0, 10001) .' -fonctionne'. '<br><br>';
+echo '3-' . calcul_mensualite(45, 10001) .' -ne doit pas fonctionne'. '<br><br>';
+echo '3-' . calcul_mensualite(45, 10000) .' -ne doit pas fonctionne'. '<br><br>';
+
+echo '--------- test fonction calculerInterets($prix,$mois,$accompte,$interet) -----------'.'<br><br><br>';
+
+echo '1-' . calculerInterets(4500,12,2000,) .' -fonctionne'. '<br><br>';
 ?>
