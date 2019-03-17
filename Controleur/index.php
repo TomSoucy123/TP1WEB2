caa534a745bf1e56d543f7e39548d8ed937b03f6
TomSoucy123 <tiyo_tom@hotmail.com>
2019-03-17 14:54:34 -0400

    Update index.php

--- Controleur/index.php
+++ Controleur/index.php
@@ -140,15 +140,24 @@ return $taxes;
 
 }
 
-function listeDeroulante($tableauInteretPrixMoinsQue10000){
-        foreach($tableauInteretPrixMoinsQue10000 as $key => $value) {
+function listeDeroulante($tableauInteretPrixMoinsQue10000,$dureeDuPret){
+    foreach($tableauInteretPrixMoinsQue10000 as $cle => $valeur){
+
+        if($cle == $dureeDuPret){
+            echo '<option value="'.$cle.'"selected>'.$cle." mois-".$valeur."%".'</option>';
+            echo '<br><br>';
+            echo "rererer";
+    }
+        else{
             
-            echo '<option value="'.$key.'">'.$key." mois-".$value."%".'</option>';
+            
+            echo '<option value="'.$cle.'">'.$cle." mois-".$valeur."%".'</option>';
             echo '<br><br>';
         
             
         
     }
+}
 }
 
 /* $capitalInvesti = 5000;
